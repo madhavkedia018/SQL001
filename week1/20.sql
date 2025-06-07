a2a98da73ae083960377abd8924f1f7c8869b4e3
madhavkedia018 <127954429+madhavkedia018@users.noreply.github.com>
2025-06-08 00:41:44 +0530

    Create 20.sql

---
+++ week1/20.sql
@@ -0,0 +1,8 @@
+SELECT 
+    cr.Name AS Country,
+    SUM(soh.TotalDue) AS TotalSales
+FROM Sales.SalesOrderHeader soh
+JOIN Person.Address a ON soh.ShipToAddressID = a.AddressID
+JOIN Person.StateProvince sp ON a.StateProvinceID = sp.StateProvinceID
+JOIN Person.CountryRegion cr ON sp.CountryRegionCode = cr.CountryRegionCode
+GROUP BY cr.Name
