bf3ca1b6ec6609694ca35ad30097b5e53df34c2f
madhavkedia018 <127954429+madhavkedia018@users.noreply.github.com>
2025-06-08 00:01:16 +0530

    Create 7.sql

---
+++ week1/7.sql
@@ -0,0 +1,12 @@
+SELECT DISTINCT
+    c.CustomerID,
+    CASE 
+        WHEN c.PersonID IS NOT NULL THEN p.FirstName + ' ' + p.LastName
+        WHEN c.StoreID IS NOT NULL THEN s.Name
+        ELSE 'N/A'
+    END AS CustomerName
+FROM Sales.Customer c
+LEFT JOIN Person.Person p ON c.PersonID = p.BusinessEntityID
+LEFT JOIN Sales.Store s ON c.StoreID = s.BusinessEntityID
+JOIN Sales.SalesOrderHeader soh ON c.CustomerID = soh.CustomerID
+ORDER  BY c.CustomerID
