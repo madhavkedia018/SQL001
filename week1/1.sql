0ea9b86af10fd2d084b18f880c04ef1a4af06b3c
madhavkedia018 <127954429+madhavkedia018@users.noreply.github.com>
2025-06-07 23:39:54 +0530

    Update 1.sql

--- week1/1.sql
+++ week1/1.sql
@@ -1,4 +1,10 @@
-SELECT FirstName,MiddleName,LastName 
-FROM Person.Person P 
-INNER JOIN  Sales.Customer S
-ON P.BusinessEntityID = S.PersonID
+SELECT 
+    c.CustomerID,
+    CASE 
+        WHEN c.PersonID IS NOT NULL THEN p.FirstName + ' ' + p.LastName
+        WHEN c.StoreID IS NOT NULL THEN s.Name
+        ELSE 'N/A'
+    END AS CustomerName
+FROM Sales.Customer c
+LEFT JOIN Person.Person p ON c.PersonID = p.BusinessEntityID
+LEFT JOIN Sales.Store s ON c.StoreID = s.BusinessEntityID;
