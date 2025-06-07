7ad8bb080eb1c341ac517f2d6497833cd605fc77
madhavkedia018 <127954429+madhavkedia018@users.noreply.github.com>
2025-06-07 23:43:47 +0530

    Create 3.sql

---
+++ week1/3.sql
@@ -0,0 +1,14 @@
+SELECT 
+    c.CustomerID,
+     CASE 
+        WHEN c.PersonID IS NOT NULL THEN p.FirstName + ' ' + p.LastName
+        WHEN c.StoreID IS NOT NULL THEN s.Name
+        ELSE 'N/A'
+    END AS CustomerName,
+    a.City
+FROM Sales.Customer c
+LEFT JOIN Person.Person p ON c.PersonID = p.BusinessEntityID
+LEFT JOIN Sales.Store s ON c.StoreID = s.BusinessEntityID
+JOIN Person.BusinessEntityAddress bea ON c.PersonID = bea.BusinessEntityID
+JOIN Person.Address a ON bea.AddressID = a.AddressID
+WHERE a.City IN ('London', 'Berlin');
