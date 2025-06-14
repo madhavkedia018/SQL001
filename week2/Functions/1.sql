221080aab628fdea239bfd6ef78807d7909b9c24
madhavkedia018 <127954429+madhavkedia018@users.noreply.github.com>
2025-06-14 11:32:23 +0530

    Update 1.sql

--- week2/Functions/1.sql
+++ week2/Functions/1.sql
@@ -1,1 +1,11 @@
+CREATE FUNCTION dbo.FormatDate_MMDDYYYY (@inputDate DATETIME)
+RETURNS VARCHAR(13)
+AS
+BEGIN
+    RETURN RIGHT('0' + CAST(MONTH(@inputDate) AS VARCHAR), 2) +'/'+
+           RIGHT('0' + CAST(DAY(@inputDate) AS VARCHAR), 2) +'/'+
+           CAST(YEAR(@inputDate) AS VARCHAR)
+END
 
+
+SELECT dbo.FormatDate_MMDDYYYY(GETDATE()) as Date 
