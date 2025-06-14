b0e6f9c20088af37b46bcbe85c4853fcf201eb0e
madhavkedia018 <127954429+madhavkedia018@users.noreply.github.com>
2025-06-14 12:57:28 +0530

    Update 1.sql

--- week2/Views/1.sql
+++ week2/Views/1.sql
@@ -1,1 +1,18 @@
+CREATE VIEW vwCustomerOrders AS
+SELECT 
+    s.Name AS CompanyName,
+    soh.SalesOrderID AS OrderID,
+    soh.OrderDate,
+    p.ProductID,
+    p.Name AS ProductName,
+    sod.OrderQty AS Quantity,
+    sod.UnitPrice,
+    (sod.OrderQty * sod.UnitPrice) AS LineTotal
+FROM Sales.SalesOrderHeader soh
+JOIN Sales.SalesOrderDetail sod ON soh.SalesOrderID = sod.SalesOrderID
+JOIN Sales.Customer c ON soh.CustomerID = c.CustomerID
+JOIN Production.Product p ON sod.ProductID = p.ProductID
+JOIN Sales.Store s ON c.StoreID = s.BusinessEntityID
 
+
+SELECT * FROM vwCustomerOrders
