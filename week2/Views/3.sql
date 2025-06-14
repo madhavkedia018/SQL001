229cbe02550e307959b510eef7f370e3004f076f
madhavkedia018 <127954429+madhavkedia018@users.noreply.github.com>
2025-06-14 14:34:22 +0530

    Create 3.sql

---
+++ week2/Views/3.sql
@@ -0,0 +1,15 @@
+CREATE VIEW MyProducts AS
+SELECT 
+    p.ProductID,
+    p.ProductName,
+    p.QuantityPerUnit,
+    p.UnitPrice,
+    s.CompanyName,
+    c.CategoryName
+FROM Products p
+JOIN Suppliers s ON p.SupplierID = s.SupplierID
+JOIN Categories c ON p.CategoryID = c.CategoryID
+WHERE p.Discontinued = 0
+
+
+SELECT * FROM MyProducts
