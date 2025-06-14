2d45f19630d78b8c15137541a107e0afcd1583b6
madhavkedia018 <127954429+madhavkedia018@users.noreply.github.com>
2025-06-14 15:08:12 +0530

    Create 1.sql

---
+++ week2/Stored_Procedures/1.sql
@@ -0,0 +1,44 @@
+CREATE PROCEDURE InsertOrderDetails
+    @OrderID INT,
+    @ProductID INT,
+    @UnitPrice MONEY = NULL,
+    @Quantity SMALLINT,
+    @Discount REAL = 0
+AS
+BEGIN
+
+    IF @UnitPrice IS NULL
+    BEGIN
+        SELECT @UnitPrice = UnitPrice
+        FROM Products
+        WHERE ProductID = @ProductID;
+    END
+
+
+    SELECT @CurrentStock = UnitsInStock, @ReorderLevel = ReorderLevel
+    FROM Products
+    WHERE ProductID = @ProductID
+
+    IF @CurrentStock IS NOT NULL AND @CurrentStock >= @Quantity
+        BEGIN
+            INSERT INTO OrderDetails (OrderID, ProductID, UnitPrice, Quantity, Discount)
+            VALUES (@OrderID, @ProductID, @UnitPrice, @Quantity, @Discount)
+
+            IF @@ROWCOUNT = 0
+                PRINT 'Failed to place the order. Please try again.'
+            ELSE 
+               BEGIN
+                      UPDATE Products
+                      SET UnitsInStock = UnitsInStock - @Quantity
+                      WHERE ProductID = @ProductID
+
+                      DECLARE @NewStock INT;
+                      SELECT @NewStock = UnitsInStock
+                      FROM Products
+                      WHERE ProductID = @ProductID
+
+                      IF @NewStock < @ReorderLevel
+                          PRINT 'Warning: Stock for this product has dropped below the reorder level.'
+               END
+        END
+END
