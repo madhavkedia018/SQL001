04c8cb90d482037b72ac3c6f65e0a36edadce714
madhavkedia018 <127954429+madhavkedia018@users.noreply.github.com>
2025-06-15 11:52:59 +0530

    Update 2.sql

--- week2/Stored_Procedures/2.sql
+++ week2/Stored_Procedures/2.sql
@@ -7,7 +7,7 @@ CREATE PROCEDURE UpdateOrderDetails
 AS
 BEGIN
     
-    -- Update UnitPrice if provided
+
     IF @UnitPrice IS NOT NULL
     BEGIN
         UPDATE OrderDetails
@@ -15,25 +15,23 @@ BEGIN
         WHERE OrderID = @OrderID AND ProductID = @ProductID
     END
 
-    -- Update Quantity if provided
     IF @Quantity IS NOT NULL
     BEGIN
         SELECT @OldQuantity = Quantity
         FROM OrderDetails
         WHERE OrderID = @OrderID AND ProductID = @ProductID
 
-        -- Update OrderDetails
         UPDATE OrderDetails
         SET Quantity = @Quantity
         WHERE OrderID = @OrderID AND ProductID = @ProductID
 
-        -- Adjust stock: Add back old, subtract new
+
         UPDATE Products
         SET UnitsInStock = UnitsInStock + @OldQuantity - @Quantity
         WHERE ProductID = @ProductID
     END
 
-    -- Update Discount if provided
+
     IF @Discount IS NOT NULL
     BEGIN
         UPDATE OrderDetails
