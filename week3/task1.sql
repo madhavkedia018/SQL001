d95e464e0edf71f5f2d5d8f42a6141caef838a0f
madhavkedia018 <127954429+madhavkedia018@users.noreply.github.com>
2025-06-21 13:13:51 +0530

    Update task1.sql

--- week3/task1.sql
+++ week3/task1.sql
@@ -8,6 +8,7 @@ insert into projects_task1 values(5,'2015-10-14','2015-10-15')
 insert into projects_task1 values(6,'2015-10-28','2015-10-29')
 insert into projects_task1 values(7,'2015-10-30','2015-10-31')
 
+  
 WITH ProjectGroups AS (
   SELECT *,
          DATEADD(DAY, -ROW_NUMBER() OVER (ORDER BY start_date), start_date) AS grp
@@ -17,7 +18,7 @@ GroupedProjects AS (
   SELECT 
     MIN(start_date) AS project_start,
     MAX(end_date) AS project_end,
-    DATEDIFF(DAY, MIN(start_date), MAX(end_date)) + 1 AS duration
+    DATEDIFF(DAY, MIN(start_date), MAX(end_date)) AS duration
   FROM ProjectGroups
   GROUP BY grp
 )
