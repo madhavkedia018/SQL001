a1aa4405b5e80f770b1be1b0e145bf230c21c699
madhavkedia018 <127954429+madhavkedia018@users.noreply.github.com>
2025-06-21 17:03:25 +0530

    Update task11.sql

--- week3/task11.sql
+++ week3/task11.sql
@@ -1,1 +1,7 @@
-
+SELECT S.Name
+FROM students S
+JOIN friends F ON S.id = F.id
+JOIN packages P1 ON S.id = P1.id        
+JOIN packages P2 ON F.friend_id = P2.id  
+WHERE P2.salary > P1.salary
+ORDER BY P2.salary
