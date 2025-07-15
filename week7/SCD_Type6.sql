2b2c7825ec45ddcefc9fc7520a9cdd931139e098
madhavkedia018 <127954429+madhavkedia018@users.noreply.github.com>
2025-07-15 22:23:03 +0530

    Create SCD_Type6.sql

---
+++ week7/SCD_Type6.sql
@@ -0,0 +1,22 @@
+CREATE PROCEDURE Load_SCD_Type_6
+AS
+BEGIN
+    DECLARE @Today DATE = GETDATE()
+
+    UPDATE DimCustomer
+    SET EndDate = @Today, CurrentFlag = 0
+    FROM DimCustomer d
+    JOIN StgCustomer s ON d.CustomerID = s.CustomerID
+    WHERE d.CurrentFlag = 1 AND ( d.Name <> s.Name OR d.Address <> s.Address OR d.City <> s.City )
+
+    INSERT INTO DimCustomer (CustomerID, Name, Address, PreviousAddress, City, StartDate, EndDate, CurrentFlag)
+    SELECT s.CustomerID, s.Name, s.Address, d.Address, s.City, @Today, NULL, 1
+    FROM StgCustomer s
+    JOIN DimCustomer d ON s.CustomerID = d.CustomerID
+    WHERE d.CurrentFlag = 0 AND d.EndDate = @Today
+
+    INSERT INTO DimCustomer (CustomerID, Name, Address, City, StartDate, EndDate, CurrentFlag)
+    SELECT s.CustomerID, s.Name, s.Address, s.City, @Today, NULL, 1
+    FROM StgCustomer s
+    WHERE NOT EXISTS ( SELECT 1 FROM DimCustomer d WHERE d.CustomerID = s.CustomerID )
+END
