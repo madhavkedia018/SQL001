28933935239959e95fde5f10e38f46645098594c
madhavkedia018 <127954429+madhavkedia018@users.noreply.github.com>
2025-07-15 22:18:04 +0530

    Update SCD_Type1.sql

--- week7/SCD_Type1.sql
+++ week7/SCD_Type1.sql
@@ -14,4 +14,4 @@ BEGIN
     WHEN NOT MATCHED BY TARGET
     THEN INSERT (CustomerID, Name, Address, City, StartDate, EndDate, CurrentFlag)
          VALUES (source.CustomerID, source.Name, source.Address, source.City, GETDATE(), NULL, 1)
-END
+END;
