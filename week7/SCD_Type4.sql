412e65ea5bd88b4153e7d81cc0f748869c7ae3c0
madhavkedia018 <127954429+madhavkedia018@users.noreply.github.com>
2025-07-15 22:21:14 +0530

    Create SCD_Type4.sql

---
+++ week7/SCD_Type4.sql
@@ -0,0 +1,19 @@
+CREATE OR ALTER PROCEDURE Load_SCD_Type_4
+AS
+BEGIN
+    INSERT INTO DimCustomer_History (CustomerID, Name, Address, City, ChangeDate)
+    SELECT d.CustomerID, d.Name, d.Address, d.City, GETDATE()
+    FROM DimCustomer d
+    JOIN StgCustomer s ON d.CustomerID = s.CustomerID
+    WHERE d.Name <> s.Name OR d.Address <> s.Address OR d.City <> s.City
+
+    UPDATE DimCustomer
+    SET Name = s.Name, Address = s.Address, City = s.City
+    FROM DimCustomer d
+    JOIN StgCustomer s ON d.CustomerID = s.CustomerID
+
+    INSERT INTO DimCustomer (CustomerID, Name, Address, City, StartDate, EndDate, CurrentFlag)
+    SELECT s.CustomerID, s.Name, s.Address, s.City, GETDATE(), NULL, 1
+    FROM StgCustomer s
+    WHERE NOT EXISTS ( SELECT 1 FROM DimCustomer d WHERE d.CustomerID = s.CustomerID )
+END
